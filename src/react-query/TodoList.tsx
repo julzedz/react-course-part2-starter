c76476dd198dc876a2b62796a906e7294fbfbb33
julzedz <edoziechukwuma@gmail.com>
2024-08-13 10:25:42 +0100

    Fetching Data

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
@@ -9,21 +10,20 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
+  const fetchTodos = () => 
+      axios
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+        .then(response => response.data)
+  const { data: todos } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos
+  });
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+  // if (error) return <p>{error}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
