0208d55a53d71720f5a6fa8e0f8b804eba5bcacf
julzedz <edoziechukwuma@gmail.com>
2024-09-24 17:40:56 +0100

    Refactor: use Reducers to manage state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 import TodoList from "./react-query/TodoList";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
+import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
+    <TaskList />
   );
 }
 
