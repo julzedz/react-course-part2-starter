1d80246a1f6c66369ad6aebb7093d51a6a48c128
julzedz <edoziechukwuma@gmail.com>
2024-09-25 16:26:17 +0100

    Refactor: Handle login state with reducers.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import Counter from "./state-management/Counter";
 import TaskList from "./state-management/TaskList";
+import LoginStatus from "./state-management/LoginStatus";
 
 function App() {
   return (
-    <TaskList />
+    <LoginStatus />
   );
 }
 
