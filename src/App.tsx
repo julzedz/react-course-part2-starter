08bd5569d90a9530e29771bbd36a9d94b0a0bfd8
julzedz <edoziechukwuma@gmail.com>
2024-09-27 12:28:05 +0100

    Share user state using Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import tasksReducer from "./state-management/reducers/tasksReducer";
+import authReducer from "./state-management/reducers/authReducer";
+import AuthContext from "./state-management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, TasksDispatch] = useReducer(tasksReducer, []);
+  const [user, AuthDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      <NavBar />
-      <HomePage />
-    </TasksContext.Provider>
+    <AuthContext.Provider value={{ user, dispatch: AuthDispatch }}>
+      <TasksContext.Provider value={{ tasks, dispatch: TasksDispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
