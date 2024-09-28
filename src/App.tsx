2fb34baa88f121f89064e3fa413067177258cc4d
julzedz <edoziechukwuma@gmail.com>
2024-09-28 01:27:40 +0100

    Refactor: Create a hook to access Context and organise imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import tasksReducer from "./state-management/reducers/tasksReducer";
-import authReducer from "./state-management/reducers/authReducer";
-import AuthContext from "./state-management/contexts/authContext";
-import AuthProvider from "./state-management/authProvider";
 
 function App() {
   const [tasks, TasksDispatch] = useReducer(tasksReducer, []);
